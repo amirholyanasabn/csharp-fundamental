e9a4a250a1124e621065654495b4ad357d239996
amirholyanasabn@outlook.com <amirholyanasabn@outlook.com>
2022-07-30 17:49:56 +0430

    inheritance and casting

--- InheritanceDemo/Person.cs
+++ InheritanceDemo/Person.cs
@@ -8,8 +8,25 @@ namespace InheritanceDemo
 {
     class Person
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Family { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            if (this.Name == (obj as Person).Name && this.Family == ((Person)obj).Family )
+                return true;
+            return false;
+           // return this.GetHashCode() == obj.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id;
+        }
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Family}";
+        }
     }
 }
--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -11,12 +11,34 @@ namespace InheritanceDemo
         static void Main(string[] args)
         {
             Person p = new Person();
-            p.Name = "John";
+            p.Id = 10;
+            p.Name = "Johnan";
             p.Family = "Doe";
-            Console.WriteLine(p.Equals(p));
+            Person p1 = new Person()
+            {
+                Id = 11,
+                Name = "John",
+                Family = "Doe"
+            };
+            Console.WriteLine(p.Equals(p1));
             Console.WriteLine(p.ToString());
             Console.WriteLine(p.GetType());
             Console.WriteLine(p.GetHashCode());
+            Console.WriteLine("----------------------------");
+            List<Person> people = new List<Person>();
+            people.Add(new Person { Name = "Amirhossein", Family = "Olyanasab", Id = 101 });
+            people.Add(new Person { Name = "Gucci", Family = "Bano", Id = 102 });
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine(people[i]);
+            }
+
+            Console.WriteLine("***********************************");
+            people.Remove(new Person { Name = "Neda", Family = "Maryami", Id = 102 });
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine(people[i]);
+            }
             Console.ReadKey();
         }
     }
