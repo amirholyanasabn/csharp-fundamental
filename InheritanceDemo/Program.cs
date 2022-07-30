3d72b4de1ff5b4a547094df744a03ca787266f74
amirholyanasabn@outlook.com <amirholyanasabn@outlook.com>
2022-07-30 16:02:02 +0430

    Add project files.

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "John";
+            p.Family = "Doe";
+            Console.WriteLine(p.Equals(p));
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p.GetType());
+            Console.WriteLine(p.GetHashCode());
+            Console.ReadKey();
+        }
+    }
+}
